e73c042644288a6413c123ef7c937fab4962ddf0
chenguang <545318542@qq.com>
2021-07-02 19:10:48 +0800

    添加V4L2，能够显示图像

--- TestV4L2/showvideo.h
+++ TestV4L2/showvideo.h
@@ -5,6 +5,11 @@
 #include <QLabel>
 #include <QImage>
 #include <QTimer>
+#include <QPaintEvent>
+#include <QHBoxLayout>
+#include <QDebug>
+
+#include "videodevice.h"
 
 
 class showvideo : public QWidget
@@ -12,10 +17,26 @@ class showvideo : public QWidget
     Q_OBJECT
 public:
     explicit showvideo(QWidget *parent = nullptr);
+    ~showvideo();
+
+    int convert_yuv_to_rgb_pixel(int y, int u, int v);
+
+    int convert_yuv_to_rgb_buffer(unsigned char *yuv, unsigned char *rgb, unsigned int width, unsigned int height);
 
 signals:
 
 public slots:
+    void paintEvent(QPaintEvent *);
+
+private:
+    videodevice *vd;   //videodevice 对象
+    QLabel *label;     //用于显示视频图片
+    QImage *img;     //图片
+    QTimer *timer;     //定时器
+    uchar  *pp;        //转换之后的视频数据
+    uchar  *p;         //存放获取的原始视频数据
+    int    len;        //存放原始视频数据长度
+
 };
 
 #endif // SHOWVIDEO_H
--- TestV4L2/videodevice.cpp
+++ TestV4L2/videodevice.cpp
@@ -107,29 +107,124 @@ int videodevice::init_mmap()
     */
     this->buffers = (VideoBuffer *)calloc(req.count, sizeof (VideoBuffer));
 
+    unsigned int numBufs = 0;
+    for (numBufs = 0; numBufs < req.count; numBufs++) //映射所有的缓存
+    {
+        struct v4l2_buffer buf;    //驱动中的一帧，保存内核中缓冲帧的相关信息结构体
+        memset(&buf, 0 , sizeof (buf));
+        //数据流类型，永远都是V4L2_BUG_TYPE_VIDEO_CAPTURE
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+        //存储类型：V4L2_MEMORY_MMAP（内存映射）或 V4L2_MEMORY_USERPTR (用户指针)
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = numBufs;    //帧号
+        //获得相应帧号的缓冲帧信息，并保存在v4l2_buf结构体变量buf中
+        if (-1 == ioctl(fd, VIDIOC_QUERYBUF, &buf))
+        {
+            perror("ioctl VIDIOC_QUERYBUF error");
+            return -1;
+        }
+
+        buffers[numBufs].length = buf.length;   //用户空间的长度
+        //使用mmap函数将申请的缓存帧地址映射到用户空间
+        buffers[numBufs].start = (unsigned char *)mmap(nullptr,buf.length,PROT_READ | PROT_WRITE,
+                                                       MAP_SHARED,fd,buf.m.offset);
+        if (buffers[numBufs].start == MAP_FAILED) {
+            perror("mmap error \n");
+            return -1;
+        }
+
+    }
+    return 0;
+
 }
 
-int videodevice::init_v4l2()
+int videodevice::init_v4l2()            //初始化 v4l2 设备
 {
+    if (-1 == init_device()) return -1;  //初始化设备
+    if (-1 == init_mmap())   return -1;  //初始化内存
 
+    return 0;
 }
 
-int videodevice::start_capturing()
+int videodevice::start_capturing()      //开始视频采集
 {
+    unsigned int i;
+    for ( i = 0; i< iCount; ++i)
+    {
+        struct v4l2_buffer buf;
+        memset(&buf, 0, sizeof (buf));
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
 
+        //将申请到的帧缓冲入队列，以便存储采集到的数据
+        if (-1 == ioctl(fd,VIDIOC_QBUF,&buf))
+        {
+            perror("ioctl VIDIOC_QBUF error");
+            return -1;
+        }
+    }
+
+    enum v4l2_buf_type type;          //开始视频采集
+    //数据流类型，永远都是 V4L2_BUF_TYPE_VIDEO_CAPTURE
+    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (ioctl(fd, VIDIOC_STREAMON,&type) < 0)  //采集视频
+    {
+        perror("ioctl VIDIOC_STREAMON error");
+        return  -1;
+    }
+    return 0;
 }
 
-int videodevice::stopt_capturing()
+int videodevice::stopt_capturing()   //停止视频采集
 {
-
+    enum v4l2_buf_type type;
+    //数据流类型，永远都是 V4L2_BUF_TYPE_VIDEO_CAPTURE
+    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (ioctl(fd, VIDIOC_STREAMOFF, &type) < 0)
+    {
+        perror("ioctl VIDIOC_STREAMOFF error");
+        return  -1;
+    }
+    return  0;
 }
 
-int videodevice::get_frame(void **, int *)
+int videodevice::get_frame(void **frame_buf, int * len)
 {
+    struct v4l2_buffer buf;
+    memset(&buf, 0, sizeof (buf));
+    //数据流类型，永远都是 V4L2_BUF_TYPE_VIDEO_CAPTURE
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    //存储类型：V4L2_MEMORY_MMAP（内存映射）或 V4L2_MEMORY_USERPTR (用户指针)
+    buf.memory = V4L2_MEMORY_MMAP;
+    if (ioctl(fd,VIDIOC_DQBUF,&buf) < 0)  //从缓存队列取出一个已经保存有一个帧视频数据的缓冲区
+    {
+        perror("ioctl VIDIOC_DQBUF failed");
+        return -1;
+    }
+    *frame_buf = buffers[buf.index].start;
+    *len = buffers[buf.index].length;
+    iIndex = buf.index;
 
+    return 0;
 }
 
 int videodevice::unget_frame()
 {
-
+    if (iIndex != -1) {
+        struct v4l2_buffer buf;
+        memset(&buf, 0, sizeof (buf));
+        //数据流类型，永远都是 V4L2_BUF_TYPE_VIDEO_CAPTURE
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        //存储类型：V4L2_MEMORY_MMAP（内存映射）或 V4L2_MEMORY_USERPTR (用户指针)
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = iIndex;
+        if (ioctl(fd, VIDIOC_QBUF, &buf) < 0)
+        {
+            return -1;
+        }
+        return 0;
+    }
+    return  -1;
 }
